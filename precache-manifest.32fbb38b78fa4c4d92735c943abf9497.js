ab7ccdcd83fdd773201ec891f54a43e9f34bef57
Airwaves <prairie5270@gmail.com>
2019-11-08 23:35:01 +0800

    Updates

---
+++ precache-manifest.32fbb38b78fa4c4d92735c943abf9497.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731c1b6af99e02a912aa06ec2d695bc1",
+    "url": "/the-f2e-2nd-freecell/index.html"
+  },
+  {
+    "revision": "db758551c14aadcae6ec",
+    "url": "/the-f2e-2nd-freecell/static/css/main.a6ec107f.chunk.css"
+  },
+  {
+    "revision": "8336061d7ce3a50d123c",
+    "url": "/the-f2e-2nd-freecell/static/js/2.a0b7d5cf.chunk.js"
+  },
+  {
+    "revision": "db758551c14aadcae6ec",
+    "url": "/the-f2e-2nd-freecell/static/js/main.94920399.chunk.js"
+  },
+  {
+    "revision": "bfd3223c64bb46c8c233",
+    "url": "/the-f2e-2nd-freecell/static/js/runtime-main.3386faaf.js"
+  },
+  {
+    "revision": "d98b0a8459157e40e107b299d59295a9",
+    "url": "/the-f2e-2nd-freecell/static/media/10_Clubs.d98b0a84.svg"
+  },
+  {
+    "revision": "4f39720c0df9269a9ff31bc09bdc6114",
+    "url": "/the-f2e-2nd-freecell/static/media/10_Diamonds.4f39720c.svg"
+  },
+  {
+    "revision": "74b1e4e428b4f9a81c8bcd0770ffefbe",
+    "url": "/the-f2e-2nd-freecell/static/media/10_Hearts.74b1e4e4.svg"
+  },
+  {
+    "revision": "6187438730768ee46c18c552256dca42",
+    "url": "/the-f2e-2nd-freecell/static/media/10_Spades.61874387.svg"
+  },
+  {
+    "revision": "e4e26db0ebd1a7f44c34ee204f7090c9",
+    "url": "/the-f2e-2nd-freecell/static/media/2_Clubs.e4e26db0.svg"
+  },
+  {
+    "revision": "6d65011442ccae06cfb97390a8646439",
+    "url": "/the-f2e-2nd-freecell/static/media/2_Diamonds.6d650114.svg"
+  },
+  {
+    "revision": "fb185d4a40072eaec366583490f84122",
+    "url": "/the-f2e-2nd-freecell/static/media/2_Hearts.fb185d4a.svg"
+  },
+  {
+    "revision": "43f683bdcda69424eaaec2c81fc614ea",
+    "url": "/the-f2e-2nd-freecell/static/media/2_Spades.43f683bd.svg"
+  },
+  {
+    "revision": "892db90b3d6d54797fc5c31a709b9989",
+    "url": "/the-f2e-2nd-freecell/static/media/3_Clubs.892db90b.svg"
+  },
+  {
+    "revision": "bebcc8b69ab634772cc335f93b2cf7d5",
+    "url": "/the-f2e-2nd-freecell/static/media/3_Diamonds.bebcc8b6.svg"
+  },
+  {
+    "revision": "f606a128af66775ab816d19f474c7897",
+    "url": "/the-f2e-2nd-freecell/static/media/3_Hearts.f606a128.svg"
+  },
+  {
+    "revision": "858145f6948bf85f524d9bb498c96df5",
+    "url": "/the-f2e-2nd-freecell/static/media/3_Spades.858145f6.svg"
+  },
+  {
+    "revision": "5b90a8c747d98a8288b621dd016f54ec",
+    "url": "/the-f2e-2nd-freecell/static/media/4_Clubs.5b90a8c7.svg"
+  },
+  {
+    "revision": "251f67f394d6fe081af1e8a00fdbd947",
+    "url": "/the-f2e-2nd-freecell/static/media/4_Diamonds.251f67f3.svg"
+  },
+  {
+    "revision": "ac9215b0df0b8448ce59a217d1e10ef5",
+    "url": "/the-f2e-2nd-freecell/static/media/4_Hearts.ac9215b0.svg"
+  },
+  {
+    "revision": "588dcc75f880fe3108f1c5ad43359284",
+    "url": "/the-f2e-2nd-freecell/static/media/4_Spades.588dcc75.svg"
+  },
+  {
+    "revision": "47f6c983eb6d2ad94c8f55df0973d42f",
+    "url": "/the-f2e-2nd-freecell/static/media/5_Clubs.47f6c983.svg"
+  },
+  {
+    "revision": "0ba28f45d7490391b438ea53c63ea4fa",
+    "url": "/the-f2e-2nd-freecell/static/media/5_Diamonds.0ba28f45.svg"
+  },
+  {
+    "revision": "68ee58f68ef93f5afd8dbf0ac2adcbd3",
+    "url": "/the-f2e-2nd-freecell/static/media/5_Hearts.68ee58f6.svg"
+  },
+  {
+    "revision": "3c34f45479dd1f24068d378e966021f5",
+    "url": "/the-f2e-2nd-freecell/static/media/5_Spades.3c34f454.svg"
+  },
+  {
+    "revision": "488324869c0e3ad192b504563d56e43f",
+    "url": "/the-f2e-2nd-freecell/static/media/6_Clubs.48832486.svg"
+  },
+  {
+    "revision": "4d9a975a0de1e9abfa47c3292298baec",
+    "url": "/the-f2e-2nd-freecell/static/media/6_Diamonds.4d9a975a.svg"
+  },
+  {
+    "revision": "5f182f504d4c28cb489a042e2e69fc43",
+    "url": "/the-f2e-2nd-freecell/static/media/6_Hearts.5f182f50.svg"
+  },
+  {
+    "revision": "0b965a7e99360d3bbe245c12262dff10",
+    "url": "/the-f2e-2nd-freecell/static/media/6_Spades.0b965a7e.svg"
+  },
+  {
+    "revision": "2ef336e9240b63e54919ce0357f900ce",
+    "url": "/the-f2e-2nd-freecell/static/media/7_Clubs.2ef336e9.svg"
+  },
+  {
+    "revision": "5331b79429130d472fdf9bdd20c82696",
+    "url": "/the-f2e-2nd-freecell/static/media/7_Diamonds.5331b794.svg"
+  },
+  {
+    "revision": "f5872bbf737e178b70b8b2c351b594d3",
+    "url": "/the-f2e-2nd-freecell/static/media/7_Hearts.f5872bbf.svg"
+  },
+  {
+    "revision": "11c46e388a4002d7b19f366bb730fd94",
+    "url": "/the-f2e-2nd-freecell/static/media/7_Spades.11c46e38.svg"
+  },
+  {
+    "revision": "d4104ecce89c5e41fb33e5f828a51576",
+    "url": "/the-f2e-2nd-freecell/static/media/8_Clubs.d4104ecc.svg"
+  },
+  {
+    "revision": "2a6e62cb0945a26ddcf9942d1b7354e3",
+    "url": "/the-f2e-2nd-freecell/static/media/8_Diamonds.2a6e62cb.svg"
+  },
+  {
+    "revision": "3d92aa6026b00eb13e868ac903577dfd",
+    "url": "/the-f2e-2nd-freecell/static/media/8_Hearts.3d92aa60.svg"
+  },
+  {
+    "revision": "8f71dc862012da02d938f4ad14976e0f",
+    "url": "/the-f2e-2nd-freecell/static/media/8_Spades.8f71dc86.svg"
+  },
+  {
+    "revision": "267dd350fdee96bc0af80e22ceba4395",
+    "url": "/the-f2e-2nd-freecell/static/media/9_Clubs.267dd350.svg"
+  },
+  {
+    "revision": "3fd0cedc16f92a5c5201aab24e9745dd",
+    "url": "/the-f2e-2nd-freecell/static/media/9_Diamonds.3fd0cedc.svg"
+  },
+  {
+    "revision": "76e62b4fd14b5739e3d104a93c0887db",
+    "url": "/the-f2e-2nd-freecell/static/media/9_Hearts.76e62b4f.svg"
+  },
+  {
+    "revision": "6399907ca58c1e3c6b43254967017cd5",
+    "url": "/the-f2e-2nd-freecell/static/media/9_Spades.6399907c.svg"
+  },
+  {
+    "revision": "ab7bb5e47ede6d3918a0ee808be1f4a0",
+    "url": "/the-f2e-2nd-freecell/static/media/A_Clubs.ab7bb5e4.svg"
+  },
+  {
+    "revision": "d4e862c233030fb6804105a7c4a723d0",
+    "url": "/the-f2e-2nd-freecell/static/media/A_Diamonds.d4e862c2.svg"
+  },
+  {
+    "revision": "304bb6ef3aaa48c7c3d08f0924041ec7",
+    "url": "/the-f2e-2nd-freecell/static/media/A_Hearts.304bb6ef.svg"
+  },
+  {
+    "revision": "ca023df27605905d1e4ed882c423398c",
+    "url": "/the-f2e-2nd-freecell/static/media/A_Spades.ca023df2.svg"
+  },
+  {
+    "revision": "e066850b3db8ddd9c522f7ec040e867c",
+    "url": "/the-f2e-2nd-freecell/static/media/Back.e066850b.svg"
+  },
+  {
+    "revision": "23cfd688b328606c267a5fa4e6d97bf1",
+    "url": "/the-f2e-2nd-freecell/static/media/Check.23cfd688.svg"
+  },
+  {
+    "revision": "3199529047e3c17ada5b2b8c44c1e2ea",
+    "url": "/the-f2e-2nd-freecell/static/media/Diamond.31995290.svg"
+  },
+  {
+    "revision": "9e78d0912c842ab46adb013b305fc4ef",
+    "url": "/the-f2e-2nd-freecell/static/media/Hint.9e78d091.svg"
+  },
+  {
+    "revision": "caa46bb523466a9a7c3147a32cdce67d",
+    "url": "/the-f2e-2nd-freecell/static/media/J_Clubs.caa46bb5.svg"
+  },
+  {
+    "revision": "65f7a17ab1c451ce7a982951299f3371",
+    "url": "/the-f2e-2nd-freecell/static/media/J_Diamonds.65f7a17a.svg"
+  },
+  {
+    "revision": "6de610fa8958996e6a2fa890113afc17",
+    "url": "/the-f2e-2nd-freecell/static/media/J_Hearts.6de610fa.svg"
+  },
+  {
+    "revision": "8b41eabe3a260d48f5ac2aab58778112",
+    "url": "/the-f2e-2nd-freecell/static/media/J_Spades.8b41eabe.svg"
+  },
+  {
+    "revision": "0133363c268e95325ce7fce747662e22",
+    "url": "/the-f2e-2nd-freecell/static/media/K_Clubs.0133363c.svg"
+  },
+  {
+    "revision": "fcb26db69c1ff11ca38f9dfbe9534907",
+    "url": "/the-f2e-2nd-freecell/static/media/K_Diamonds.fcb26db6.svg"
+  },
+  {
+    "revision": "1b5bfe889a2546f6eb03840ce70453ed",
+    "url": "/the-f2e-2nd-freecell/static/media/K_Hearts.1b5bfe88.svg"
+  },
+  {
+    "revision": "85240b9342b63beadadbb00489ab30b3",
+    "url": "/the-f2e-2nd-freecell/static/media/K_Spades.85240b93.svg"
+  },
+  {
+    "revision": "e8151770b441c1fae574d9b2d0147117",
+    "url": "/the-f2e-2nd-freecell/static/media/Plus.e8151770.svg"
+  },
+  {
+    "revision": "baf9c346274e3e5cd9907927130ee87e",
+    "url": "/the-f2e-2nd-freecell/static/media/Q_Clubs.baf9c346.svg"
+  },
+  {
+    "revision": "0dc1bf7ed29d343af5b610363d5dd184",
+    "url": "/the-f2e-2nd-freecell/static/media/Q_Diamonds.0dc1bf7e.svg"
+  },
+  {
+    "revision": "9dc93f1c949f56eba89230f005b1313e",
+    "url": "/the-f2e-2nd-freecell/static/media/Q_Hearts.9dc93f1c.svg"
+  },
+  {
+    "revision": "2d09b22acb618b2fa84b659057f89344",
+    "url": "/the-f2e-2nd-freecell/static/media/Q_Spades.2d09b22a.svg"
+  },
+  {
+    "revision": "260221fc2dbd44f25e6b9392e0b0be76",
+    "url": "/the-f2e-2nd-freecell/static/media/Time icon.260221fc.svg"
+  }
+]);
